0d97d8627d5cffd256bcd6981d655cbbada40ad6
Adam Neumann <adam@noizwaves.com>
2016-12-02 19:01:17 -0700

    Backfill unit tests for TypeaheadDirective.

---
+++ src/app/typeahead/typeahead.directive.spec.ts
@@ -0,0 +1,31 @@
+import {TypeaheadDirective} from './typeahead.directive';
+import {FormControl} from '@angular/forms';
+import {TypeaheadStrategy} from './typeahead-strategy';
+
+describe('TypeaheadDirective', () => {
+  describe('initialisation', () => {
+    it('throws exception when "strategy" input is not provided', () => {
+      let directive = new TypeaheadDirective();
+      directive.formControl = {} as FormControl;
+      directive.strategy = null;
+
+      try {
+        directive.ngOnInit();
+
+        fail('exception not thrown');
+      } catch (e) { }
+    });
+
+    it('throws exception when "formControl" input is not provided', () => {
+      let directive = new TypeaheadDirective();
+      directive.formControl = null;
+      directive.strategy = {} as TypeaheadStrategy;
+
+      try {
+        directive.ngOnInit();
+
+        fail('exception not thrown');
+      } catch (e) { }
+    });
+  });
+});
