b5c0ec42751f939544b9ac131c7171e8b5062daf
Adam Neumann <adam@noizwaves.com>
2016-12-02 18:49:51 -0700

    Perform query setting in typeahead directive itself (and not the test component).
    - this was previously being done in the TestComponent
    - directive is able to 'steal' (peek?) the [formControl] from the input directive.

--- src/app/typeahead/integration.spec.ts
+++ src/app/typeahead/integration.spec.ts
@@ -1,5 +1,5 @@
 import {TestBed, ComponentFixture} from '@angular/core/testing';
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {By} from '@angular/platform-browser';
 import {TypeaheadStrategy} from './typeahead-strategy';
@@ -53,20 +53,16 @@ describe('Directive: Typeahead', () => {
 
 @Component({
   template: `
-    <input type="text" typeahead [formControl]="stateControl"/>
+    <input type="text" [typeahead]="strategy" [formControl]="stateControl"/>
     <typeahead-items [strategy]="strategy"></typeahead-items>
 `
 })
-class TestComponent implements OnInit {
+class TestComponent {
   stateControl = new FormControl(null, Validators.required);
   strategy: TypeaheadStrategy;
 
   constructor(tb: TypeaheadBuilder) {
     this.strategy = tb.constantList(['foobar', 'foobaz']);
-  }
-
-  ngOnInit(): void {
-    this.stateControl.valueChanges.subscribe(v => this.strategy.setQuery(v));
   }
 }
 
--- src/app/typeahead/typeahead.directive.ts
+++ src/app/typeahead/typeahead.directive.ts
@@ -1,7 +1,32 @@
-import {Directive} from '@angular/core';
+import {Directive, OnInit, Input, OnDestroy} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {TypeaheadStrategy} from './typeahead-strategy';
+import {Subscription} from 'rxjs/Rx';
 
 @Directive({
   selector: '[typeahead]'
 })
-export class TypeaheadDirective {
+export class TypeaheadDirective implements OnInit, OnDestroy {
+  @Input('typeahead') strategy: TypeaheadStrategy;
+  @Input('formControl') formControl: FormControl;
+
+  private subscription: Subscription = null;
+
+  ngOnInit(): void {
+    if (!this.strategy) {
+      throw '"strategy" input is required for typeahead';
+    }
+
+    if (!this.formControl) {
+      throw '"formControl" input is required for typeahead';
+    }
+
+    this.subscription = this.formControl.valueChanges.subscribe(v => this.strategy.setQuery(v));
+  }
+
+  ngOnDestroy(): void {
+    if (!this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
